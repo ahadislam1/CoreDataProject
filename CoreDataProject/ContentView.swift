a6ab831b85e2eb92f684c302e3fecc1ab88c65b2
Ahad Islam <ahad.islam624@gmail.com>
2020-05-06 11:25:31 -0400

    going through how core data makes objects.  and how swiftUI uses \.self

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -8,9 +8,29 @@
 
 import SwiftUI
 
+/**
+ \.self means the whole object.
+ It means if we can conform hashable to our models then it'll value itself as struct.
+ 
+ CoreData also creates sequential object identifiers so it doesn't break when objects have identical properties.
+ */
+struct Student: Hashable {
+    let name: String
+}
+
+struct StudentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermoine Granger")]
+    
+    var body: some View {
+        List(students, id: \.self) { student in
+            Text(student.name)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello World!")
     }
 }
 
